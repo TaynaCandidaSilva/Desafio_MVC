57d25dadf46f17093175888ed4fe7e39b5c563d4
TaynaCandidaSilva~ <tayna2507@gmail.com>
2025-01-13 17:30:21 -0300

    Implementando teste de conexao com o banco de dados

--- src/models/sqlite/settings/connection.py
+++ src/models/sqlite/settings/connection.py
@@ -7,7 +7,11 @@ class DBConnectionHandler:
         self.__engine = None
 
     def connect_to_db(self):
+
         self.__engine = create_engine(self.__connection_string)
 
     def get_engine(self):
         return self.__engine
+
+
+db_connection_handler = DBConnectionHandler()
