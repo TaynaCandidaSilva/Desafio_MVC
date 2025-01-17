fbaa41ee10e4ff932867eb3e422111ddbb367938
TaynaCandidaSilva~ <tayna2507@gmail.com>
2025-01-17 15:11:01 -0300

    Implementacao: teste unitario de No Result em pessoa fisica

--- src/models/sqlite/repositories/pessoa_fisica_repository_test.py
+++ src/models/sqlite/repositories/pessoa_fisica_repository_test.py
@@ -1,4 +1,5 @@
 from mock_alchemy.mocking import UnifiedAlchemyMagicMock
+from sqlalchemy.orm.exc import NoResultFound
 from unittest import mock
 from src.models.sqlite.entities.pessoa_fisica import PessoaFisica
 from .pessoa_fisica_repository import PessoaFisicaRepository
@@ -28,7 +29,37 @@ class MockConnection:
 def test_list_pessoa_fisica():
     mock_connection = MockConnection()
     repo = PessoaFisicaRepository(mock_connection)
-    response = repo.listar_usuarios()
+    response = repo.listar_usuarios_pf()
+
+    mock_connection.session.query.assert_called_once_with(PessoaFisica)
+    mock_connection.session.all.assert_called_once()
+    mock_connection.session.filter.assert_not_called()
 
     assert response[0].nome_completo == "Mario"
-    assert response
+
+
+class MockConnectionNoResult:
+    def __init__(self) -> None:
+        self.session = UnifiedAlchemyMagicMock()
+        self.session.query.side_effect = self.__raise_no_result_found
+
+    def __raise_no_result_found(self, *args, **kwargs):
+        raise NoResultFound("No result found")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+def test_list_pessoa_fisica_no_result():
+    mock_connection = MockConnectionNoResult()
+    repo = PessoaFisicaRepository(mock_connection)
+    response = repo.listar_usuarios_pf()
+
+    mock_connection.session.query.assert_called_once_with(PessoaFisica)
+    mock_connection.session.all.assert_not_called()
+    mock_connection.session.filter.assert_not_called()
+
+    assert response == []
